34b6e9b52384aea52749d7e8fb12c85f00372d61
Sandiah <sandiah san>
2018-03-20 14:53:54 +0700

    createt report

---
+++ app/Http/Controllers/Report/ReportController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Report;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReportController extends Controller
+{
+    public function index()
+    {
+    	// On progress, still waiting API
+    }
+}
