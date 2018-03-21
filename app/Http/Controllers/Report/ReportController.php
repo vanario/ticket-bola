3d0485d348a6242a5742a5e6488752c07288b312
Sandiah <sandiah san>
2018-03-21 15:06:41 +0700

    get data list club, list current, and list summary

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -4,11 +4,59 @@ namespace App\Http\Controllers\Report;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Ixudra\Curl\Facades\Curl;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\Http\Controllers\Authentication\LoginController;
+use Session;
+use Alert;
 
 class ReportController extends Controller
 {
     public function index()
     {
-    	// On progress, still waiting API
+        return view('Report/index');
+    }
+
+    public function club()
+    {
+        $token = Session::get('token'); 
+
+        $response = Curl::to('128.199.161.172:8091/getbygttop/TB')
+                    ->asJson(true)
+                    ->get(); 
+
+        $data     = $response['value'];
+        
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $col = collect($data);
+        $perPage = 10;
+        $currentPageSearchResults = $col->slice(($currentPage - 1) * $perPage, $perPage)->all();
+        // $data = new LengthAwarePaginator($currentPageSearchResults, count($col), $perPage);
+        $data = new LengthAwarePaginator($currentPageSearchResults, count($col), $perPage, $currentPage,['path' => LengthAwarePaginator::resolveCurrentPath()] );
+
+        // return $data;
+
+        return view('Report/Club/club',['data' => $data,]);
+    }
+
+    public function clubReport($id)
+    {
+        $token = Session::get('token'); 
+
+        $sumCurrentResponse = Curl::to('http://128.199.161.172:8109/report/tiket/sumcurrent/'.$id)
+                    ->asJson(true)
+                    ->withHeader('Authorization:'.$token)
+                    ->get(); 
+
+        $listCurrentResponse = Curl::to('http://128.199.161.172:8109/report/tiket/listcurrent/'.$id)
+                    ->asJson(true)
+                    ->withHeader('Authorization:'.$token)
+                    ->get(); 
+
+
+        $sumCurrent     = $sumCurrentResponse['values'];
+        $listCurrent    = $listCurrentResponse['values'];
+
+        return view('Report/Club/detail', compact('sumCurrent', 'listCurrent'));
     }
 }
