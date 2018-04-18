87c5fc59a4cddfc72d428dca7aaf98ffa0e63a77
rahmat vanario <rahmatvanario27@gmail.com>
2018-04-18 11:18:49 +0700

    add view master biaya

--- resources/views/DataMaster/MasterBiaya/masterbiaya.blade.php
+++ resources/views/DataMaster/MasterBiaya/masterbiaya.blade.php
@@ -21,51 +21,45 @@
                         </thead>
 
                         <tbody>
-                            {{-- @foreach($data as $val)  
+                            @foreach($data as $val)  
                             <tr>
-                                <td>{{ $val['tanggal'] or "-"}}</td>
-                                <td>{{ $val['jadwal_pertandingan'] or "-"}}</td>
-                                <td>{{ $val['tipe_biaya'] or "-"}}</td>
-                                <td>{{ $val['nominal'] or "-"}}</td>
-                                <td>{{ $val['keterangan'] or "-"}}</td>
+                                <td>{{ $val['akunname'] or "-"}}</td>
+                                <td>{{ $val['akuntype'] or "-"}}</td>
                                 <td>
-                                    <a data-toggle="modal" data-target="#edit{{$val['gtcode']}}"><span class="fa fa-pencil" style="color:green"></span></a>      
-                            @endforeach --}}
+                                    <a data-toggle="modal" data-target="#edit{{$val['gtcode']}}"><span class="fa fa-pencil" style="color:green"></span></a>
+                                    <a href="{{action('DataMaster\MasterBiayaController@destroy',$val['gtcode'])}}" id="hapus" ><i class="fa fa-trash"></i></a>      
+                            @endforeach
                                 
                         </tbody>
                     </table>
-                    {{-- <ul class="pagination">
-                        <li><a href="{{action('Biaya\BiayaController@page', 1 )}}" rel="prev">&laquo;</a></li> 
+                    <ul class="pagination">
+                        <li><a href="{{action('DataMaster\MasterBiayaController@page', 1 )}}" rel="prev">&laquo;</a></li> 
                         @for ($i = 1; $i <= $total_page; $i++)
                             @if( $i+1 <= 15)                            
-                            <li><a href="{{action('DataMaster\MitraController@page', $i )}}" id="paging">{{$i}}</a></li>
+                            <li><a href="{{action('DataMaster\MasterBiayaController@page', $i )}}" id="paging">{{$i}}</a></li>
                             @endif
                         @endfor
-                        <li><a href="{{action('DataMaster\MitraController@page', $total_page )}}" rel="next">&raquo;</a></li>
-                    </ul> --}} 
+                        <li><a href="{{action('DataMaster\MasterBiayaController@page', $total_page )}}" rel="next">&raquo;</a></li>
+                    </ul> 
             </div>
         </div>
 
         <div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <form method="POST" action="{{ route('biaya.store') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('master-biaya.store') }}" enctype="multipart/form-data">
                            {{ csrf_field() }}
                         <div class="modal-header">
                             <h4>Tambah Biaya</h4>
                         </div>
-                        <div class="modal-body">       
+                        <div class="modal-body">     
                             <div class="form-group">
-                                <label for="">Kode</label>
-                                <input type="text" name="gtcode" id="gtcode" class="form-control input-sm" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Nama Biaya</label>
-                                <input type="text" name="nama_biaya" id="nama_biaya" class="form-control input-sm" required>
+                                <label for="">Nama Akun</label>
+                                <input type="text" name="akunname" id="akunname" class="form-control input-sm" required>
                             </div>
                             <div class="form-group">
                                 <label for="">Tipe Biaya</label>
-                                <input type="text" name="tipe_biaya" id="tipe_biaya" class="form-control input-sm" required>
+                                <input type="text" name="akuntype" id="akuntype" class="form-control input-sm" required>
                             </div>
                         <div class="modal-footer">
                             <div>
@@ -81,7 +75,7 @@
         <div class="modal fade" id="edit{{$val['gtcode']}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <form method="POST" action="{{ route('biaya.update')}}" >
+                    <form method="POST" action="{{ route('master-biaya.update')}}" >
                     {{ csrf_field() }}
                     <input name="_method" type="hidden" value="PATCH">
                         <div class="modal-header">
@@ -89,16 +83,12 @@
                         </div>
                         <div class="modal-body">         
                             <div class="form-group">
-                                <label for="">Kode</label>
-                                <input type="text" name="gtcode" id="gtcode" class="form-control input-sm" required>
-                            </div>
-                            <div class="form-group">
                                 <label for="">Nama Biaya</label>
-                                <input type="text" name="nama_biaya" id="nama_biaya" class="form-control input-sm" required>
+                                <input type="text" name="nama_biaya" value="{{$val['akunname']}}" id="nama_biaya" class="form-control input-sm" required>
                             </div>
                             <div class="form-group">
                                 <label for="">Tipe Biaya</label>
-                                <input type="text" name="tipe_biaya" id="tipe_biaya" class="form-control input-sm" required>
+                                <input type="text" name="tipe_biaya" value="{{$val['akunname']}}" id="tipe_biaya" class="form-control input-sm" required>
                             </div>
                         </div>
                         <div class="modal-footer">
