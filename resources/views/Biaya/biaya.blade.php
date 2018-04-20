aef79601139a8b97b01cae25004ce590428d1657
rahmat vanario <rahmatvanario27@gmail.com>
2018-04-20 08:35:57 +0700

    edit biaya

--- resources/views/Biaya/biaya.blade.php
+++ resources/views/Biaya/biaya.blade.php
@@ -147,5 +147,23 @@
 @include('sweet::alert')
 @endsection
 
+<script type="text/javascript">
+
+    function addElement(parentId, elementTag, elementId, html) {
+        // Adds an element to the document
+        var p = document.getElementById(parentId);
+        var newElement = document.createElement(elementTag);
+        newElement.setAttribute('id', elementId);
+        newElement.innerHTML = html;
+        p.appendChild(newElement);
+    }
+
+    function removeElement(elementId) {
+        // Removes an element from the document
+        var element = document.getElementById(elementId);
+        element.parentNode.removeChild(element);
+    }
+</script>
+
 
 
