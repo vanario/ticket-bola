29e827a0dbf3e2eda325d28e5e8eade5181e66f8
rahmat vanario <rahmatvanario27@gmail.com>
2018-05-04 15:52:22 +0700

    edit aside

--- resources/views/aside.blade.php
+++ resources/views/aside.blade.php
@@ -31,12 +31,24 @@
 
             <li><a href="{{ url('register/index') }}"><i class="fa fa-address-card-o" style="color:white"></i>Member</a></li>
 
-            <li><a href="{{ url('news/') }}"><i class="fa fa-newspaper-o" style="color:white"></i>News</a></li>
+            <li><a href="{{ url('news/') }}"><i class="fa fa-newspaper-o" style="color:white"></i>Berita</a></li>
 
             <li><a href="{{ url('merchandise') }}"><i class="fa fa-shopping-bag" style="color:white"></i>Mercendais</a></li>
 
 
-            <li><a href="{{ url('report') }}"><i class="fa fa-newspaper-o" style="color:white"></i>Report</a></li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-newspaper-o" style="color:white"></i><span>Laporan</span>
+                        <span class="pull-right-container">
+                    <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                </a>
+                <ul class="treeview-menu">                 
+                    <li><a href="{{-- {{ url('tribun/index') }} --}}"><i class="fa fa-circle-o"></i>Laporan Pendapatan</a></li>  
+                    <li><a href="{{-- {{ url('tribun/index') }} --}}"><i class="fa fa-circle-o"></i>Laporan Pertahun</a></li>  
+                    <li><a href="{{ url('report/') }}"><i class="fa fa-circle-o"></i>Laporan Penjualan Tiket</a></li>
+                </ul>
+            </li>  
 
 
             <li class="treeview">
@@ -47,7 +59,7 @@
                 </span>
                 </a>
                 <ul class="treeview-menu">                 
-                    <li><a href="{{ url('previledge/index') }}"><i class="fa fa-circle-o"></i>Hak Akses user</a></li>
+                    {{-- <li><a href="{{ url('previledge/index') }}"><i class="fa fa-circle-o"></i>Hak Akses user</a></li> --}}
                     <li><a href="{{ url('member/index') }}"><i class="fa fa-circle-o" style="color:white; margin-right: 10px;"></i>Register</a></li>
                 </ul>
             </li>
