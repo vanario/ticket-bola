3a0be7627575e04b813657a43efbac1ac570c725
vanario <rahmatvanario27@gmail.com>
2018-05-21 10:14:14 -0700

    add kategori merchandise

---
+++ app/Http/Controllers/DataMaster/MerchandiseKategoriController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\DataMaster;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MerchandiseKategoriController extends Controller
+{
+    //
+}
