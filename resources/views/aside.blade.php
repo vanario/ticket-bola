556befbeaf74911af8a922111acef7e5dfacc9bd
rahmat vanario <rahmatvanario27@gmail.com>
2018-05-23 10:57:14 +0700

    edit aside

--- resources/views/aside.blade.php
+++ resources/views/aside.blade.php
@@ -7,23 +7,7 @@
       <ul class="sidebar-menu" data-widget="tree">
 
             <li><a href="{{ url('home/') }}"><i class="fa fa-tv" style="color:white"></i>Dashboard</a></li>  
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-database" style="color:white"></i> <span>Data Master</span>
-                        <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-                </span>
-                </a>
-                <ul class="treeview-menu">                 
-                    <li><a href="{{ url('stadion/') }}"><i class="fa fa-circle-o"></i>Stadion</a></li>
-                    <li><a href="{{ url('tribun/index') }}"><i class="fa fa-circle-o"></i>Tribun </a></li>  
-                    <li><a href="{{ url('club/') }}"><i class="fa fa-circle-o"></i>Club </a></li>  
-                    <li><a href="{{ url('jadwal/') }}"><i class="fa fa-circle-o"></i>Jadwal </a></li>  
-                    <li><a href="{{ url('mitra/') }}"><i class="fa fa-circle-o"></i>Mitra </a></li>  
-                </ul>
-            </li>
-
-            <li><a href="{{ url('pertandingan') }}"><i class="fa fa-file-text-o" style="color:white"></i>Entry Pertandingan</a></li>
+            {{-- <li><a href="{{ url('pertandingan') }}"><i class="fa fa-file-text-o" style="color:white"></i>Entry Pertandingan</a></li> --}}
 
             <li><a href="{{ url('biaya') }}"><i class="fa fa-file-text-o" style="color:white"></i>Transaksi</a></li>
 
@@ -31,7 +15,7 @@
 
             <li><a href="{{ url('register/index') }}"><i class="fa fa-address-card-o" style="color:white"></i>Member</a></li>
 
-            <li><a href="{{ url('news/') }}"><i class="fa fa-newspaper-o" style="color:white"></i>Berita</a></li>
+            <li><a href="{{ url('news') }}"><i class="fa fa-newspaper-o" style="color:white"></i>Berita</a></li>
 
             <li><a href="{{ url('merchandise') }}"><i class="fa fa-shopping-bag" style="color:white"></i>Mercendais</a></li>
 
@@ -61,6 +45,22 @@
                 <ul class="treeview-menu">                 
                     {{-- <li><a href="{{ url('previledge/index') }}"><i class="fa fa-circle-o"></i>Hak Akses user</a></li> --}}
                     <li><a href="{{ url('member/index') }}"><i class="fa fa-circle-o" style="color:white; margin-right: 10px;"></i>Register</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-database" style="color:white"></i> <span>Data Master</span>
+                        <span class="pull-right-container">
+                    <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                </a>
+                <ul class="treeview-menu">                 
+                    <li><a href="{{ url('stadion/') }}"><i class="fa fa-circle-o"></i>Stadion</a></li>
+                    <li><a href="{{ url('tribun/index') }}"><i class="fa fa-circle-o"></i>Tribun </a></li>  
+                    <li><a href="{{ url('club/') }}"><i class="fa fa-circle-o"></i>Club </a></li>  
+                    <li><a href="{{ url('jadwal/') }}"><i class="fa fa-circle-o"></i>Jadwal </a></li>  
+                    <li><a href="{{ url('mitra/') }}"><i class="fa fa-circle-o"></i>Mitra </a></li>  
                 </ul>
             </li>
 
